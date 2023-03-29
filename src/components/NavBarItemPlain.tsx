6f12fbc6c52534c43d9fc77145fe2018b5b49c69
jancarlohendriks <jancarlohendriks@hotmail.com>
2023-03-29 11:26:56 +0200

    menu mobile uu logo fix

--- src/components/NavBarItemPlain.tsx
+++ src/components/NavBarItemPlain.tsx
@@ -18,7 +18,7 @@ export default function NavBarItemPlain({
   const navBarItemLabelHoverStyle = useAppSelector((state) => state.style.navBarItemLabelHoverStyle)
 
   // const classBase = 'items-center cursor-pointer dark:text-white dark:hover:text-slate-400'
-  const classBase = 'items-center cursor-pointer dark:text-white'
+  const classBase = 'max-w-xs items-center cursor-pointer dark:text-white'
   // const classAddon = `${display} ${navBarItemLabelStyle} ${navBarItemLabelHoverStyle} ${
   //   useMargin ? 'my-2 mx-3' : 'py-2 px-3'
   // }`
